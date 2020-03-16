55d6d36103f19d018b55f7be019ca7765fbf01cb
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-03-17 02:50:53 +0300

    Put all logic for fetching tweets in one place

--- feed.rkt
+++ feed.rkt
@@ -3,26 +3,22 @@
 (require simple-http)
 (require json)
 (require html-parsing)
-(require html-writing)
 (require sxml/sxpath)
 (require redis)
 
 (define (get-tweets name)
   """Get Tweets from a user's feed"""
-  (let [(requester (update-ssl (update-host json-requester "syndication.twitter.com") #t))
-        (params `((dnt . "false")
-                  (screen_name . ,name)
-                  (suppress_response_codes . "true")
-                  (lang . "en")
-                  (rnd . ,(number->string (random 10000000)))))]
-    (get
-     requester "/timeline/profile" #:params params)))
-
-(define tweets
-  (let [(html-body (html->xexp
-                    (hash-ref (json-response-body (get-tweets "AbePalmer"))
-                              'body)))
-        (query (sxpath "//p[contains(@class, 'timeline-Tweet-text')]/text()"))]
+  (let* [(requester (update-ssl (update-host json-requester "syndication.twitter.com") #t))
+         (params `((dnt . "false")
+                   (screen_name . ,name)
+                   (suppress_response_codes . "true")
+                   (lang . "en")
+                   (rnd . ,(number->string (random 10000000)))))
+         (html-body (html->xexp
+                     (hash-ref (json-response-body
+                                (get requester "/timeline/profile" #:params params))
+                               'body)))
+         (query (sxpath "//p[contains(@class, 'timeline-Tweet-text')]/text()"))]
     (query html-body)))
 
 (define (store-tweets tweets queue-name)
@@ -32,9 +28,10 @@
                     (for ([x xs])
                       (fn x))))]
     (for-each tweets (lambda (tweet)
-                       (redis-list-prepend!
+                       (redis-list-append!
                         c queue-name
                         (string->bytes/utf-8 tweet))))))
 
-;; ;;; Store the tweets
-(store-tweets tweets "AbePalmer")
+;;; Store the tweets
+(store-tweets (get-tweets "BonfaceKilz")
+              "BonfaceKilz")
