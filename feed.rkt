eb12d7992a9b390ce2fba1c9f564b628656086f3
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-03-18 02:11:31 +0300

    Store tweets as JSON to Redis

--- feed.rkt
+++ feed.rkt
@@ -1,10 +1,13 @@
 #lang racket/base
 
 (require simple-http)
-(require json)
 (require html-parsing)
+(require html-writing)
 (require sxml/sxpath)
 (require redis)
+
+(define-syntax-rule (extract-from-tweet fn path tweet)
+  (fn ((sxpath path) tweet)))
 
 (define (get-tweets name)
   """Get Tweets from a user's feed"""
@@ -13,13 +16,16 @@
                    (screen_name . ,name)
                    (suppress_response_codes . "true")
                    (lang . "en")
+                   (with_replies . "false")
+                   (hide_thread . "t")
                    (rnd . ,(number->string (random 10000000)))))
          (html-body (html->xexp
                      (hash-ref (json-response-body
                                 (get requester "/timeline/profile" #:params params))
                                'body)))
-         (query (sxpath "//p[contains(@class, 'timeline-Tweet-text')]/text()"))]
-    (query html-body)))
+         (query-tweets (sxpath "//li[contains(@class, 'timeline-Tweet')]/div[contains(@class, 'timeline-Tweet')]"))]
+    (query-tweets html-body)))
+
 
 (define (store-tweets tweets queue-name)
   """Store tweets to a list"
@@ -28,10 +34,19 @@
                     (for ([x xs])
                       (fn x))))]
     (for-each tweets (lambda (tweet)
-                       (redis-list-append!
-                        c queue-name
-                        (string->bytes/utf-8 tweet))))))
+                       (let [(author (extract-from-tweet car "//span[contains(@class, 'TweetAuthor-name')]/text()" tweet))
+                             (time-posted (extract-from-tweet cadar "//time/@title" tweet))
+                             (raw-html (extract-from-tweet xexp->html "//p[contains(@class, 'timeline-Tweet-text')]" tweet))]
+                         (redis-list-append!
+                          c queue-name
+                          (string->bytes/utf-8 (string-append
+                                                "{'author': '"
+                                                author
+                                                "' , 'time': '"
+                                                time-posted
+                                                "' , 'tweet': '"
+                                                raw-html
+                                                "' }"))))))))
 
 ;;; Store the tweets
-(store-tweets (get-tweets "AbePalmer")
-              "AbePalmer")
+(store-tweets (get-tweets "AbePalmer") "AbePalmer")
