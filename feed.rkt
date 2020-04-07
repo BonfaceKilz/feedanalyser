ad3cfd749e1e099d7381d08773921365a96109f4
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-04-07 17:56:11 +0300

    Add way of storing GH commits from a repository

--- feed.rkt
+++ feed.rkt
@@ -9,6 +9,8 @@
 (require sxml/sxpath)
 (require json)
 (require redis)
+(require lens/common)
+(require lens/data/hash)
 
 (define-syntax-rule (extract-from-tweet fn path tweet)
   (fn ((sxpath path) tweet)))
@@ -33,6 +35,13 @@
                                'body)))
          (query-tweets (sxpath "//li[contains(@class, 'timeline-Tweet')]/div[contains(@class, 'timeline-Tweet')]"))]
     (query-tweets html-body)))
+
+(define (ghcommits->hash repo-name)
+  """Get commits from a repo"""
+  (let* [(requester (update-ssl (update-host json-requester "api.github.com") #t))
+         (params `'((page . "1") (per_page . "10")))]
+    (json-response-body
+     (get requester "/repos/graph-genome/Schematize/commits" #:params params))))
 
 (define (tweet->json tweet)
   "Convert a single tweet to a valid JSON object"
@@ -65,6 +74,18 @@
               (filter filter-fn
                       (get-tweets name)))))
 
+
+(define (store-gh-commits repository)
+  """Store commits to a list"
+  (let [(c (make-redis))
+        (commit-lens (lens-compose (hash-pick-lens 'author 'message 'url)
+                                   (hash-ref-lens 'commit)))]
+    (for-each (lambda (commit)
+                (redis-list-append!
+                 c
+                 "Github"
+                 (jsexpr->bytes (lens-view commit-lens commit))))
+              (ghcommits->hash repository))))
 
 (define (list->regex-string xs)
   """Given a list of strings, generate a valid regex string separated by '|' """
