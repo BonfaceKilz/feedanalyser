630e11ec4214094eede75a8716a32a1f8205737c
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-06-15 22:48:14 +0300

    Inject the REDIS client when starting the server

--- feeds/private/server.rkt
+++ feeds/private/server.rkt
@@ -9,23 +9,18 @@
 
 (provide start-server)
 
-;; TODO: Get configs from file
-(define c (make-redis))
-
-(get "/"
-     (lambda (req)
-       (let ([google-font-link "https://fonts.googleapis.com/css2?family=Amatic+SC&family=Josefin+Sans:ital,wght@1,300&display=swap"]
-             [tweets (get-tweets/redis c)])
-         (include-template "templates/polling.html"))))
-
-(post "/vote"
-      (lambda (req)
-        (let* ([json/vals (bytes->jsexpr (request-post-data/raw req))]
-               [tweet-hash (hash-ref json/vals 'tweet-hash)]
-               [upvote (hash-ref json/vals 'upvote)])
-          (vote-tweet c tweet-hash #:upvote? (string=? upvote "upvote"))
-          "OK")))
-
-(define start-server
-  (lambda()
-    (run)))
+;; When starting the server, inject, the redis client
+(define (start-server client)
+  (get "/"
+       (lambda (req)
+         (let ([google-font-link "https://fonts.googleapis.com/css2?family=Amatic+SC&family=Josefin+Sans:ital,wght@1,300&display=swap"]
+               [tweets (get-tweets/redis client)])
+           (include-template "templates/polling.html"))))
+  (post "/vote"
+        (lambda (req)
+          (let* ([json/vals (bytes->jsexpr (request-post-data/raw req))]
+                 [tweet-hash (hash-ref json/vals 'tweet-hash)]
+                 [upvote (hash-ref json/vals 'upvote)])
+            (vote-tweet client tweet-hash #:upvote? (string=? upvote "upvote"))
+            "OK")))
+  (run))
