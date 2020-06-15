bcf32b75f07bc24706d5349ae4d3750c3510bac2
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-06-16 00:16:26 +0300

    Add optional params for port and log-file during server start

--- feeds/private/server.rkt
+++ feeds/private/server.rkt
@@ -9,8 +9,8 @@
 
 (provide start-server)
 
-;; When starting the server, inject, the redis client
-(define (start-server client)
+;; When starting the server, inject, the REDIS client
+(define (start-server client #:port [port 8000] #:log-file [log-file "feed.log"])
   (get "/"
        (lambda (req)
          (let ([google-font-link "https://fonts.googleapis.com/css2?family=Amatic+SC&family=Josefin+Sans:ital,wght@1,300&display=swap"]
@@ -23,4 +23,5 @@
                  [upvote (hash-ref json/vals 'upvote)])
             (vote-tweet client tweet-hash #:upvote? (string=? upvote "upvote"))
             "OK")))
-  (run))
+  (displayln (string-append "Running the server on port " (number->string port)))
+  (run #:port port #:log-file log-file))
