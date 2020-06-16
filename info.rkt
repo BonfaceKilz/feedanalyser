5aa121a3ec9a375e7d1551223d82e618503346cb
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-06-16 15:39:28 +0300

    Restructure project
    Makes it easy to install from git

--- info.rkt
+++ info.rkt
@@ -1,14 +1,15 @@
-#lang info
+#lang setup/infotab
 
-(define version "0.0.0")
 (define name "feeds")
 (define blurb
   (list '(p "Fetch feeds from different places in Racket")))
 (define primary-file "main.rkt")
 (define categories '(net))
+(define version "0.0.0")
 (define deps '("base"
                "redis-rkt"
                "gregor"
                "https://github.com/dmac/spin.git"
                "web-server-lib"))
 (define build-deps '())
+
