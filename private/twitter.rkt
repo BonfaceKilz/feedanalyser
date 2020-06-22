a9c2e4fe5356d8cc42f63b3c2cb2f014b58528f8
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-06-22 08:17:57 +0300

    Remove shadowing of `client` variable in nested function

--- private/twitter.rkt
+++ private/twitter.rkt
@@ -105,7 +105,7 @@
 
 ;; Given a list of feed-tweets, store them in REDIS
 (define (store-tweets client tweets)
-  (define (store-tweet client tweet)
+  (define (store-tweet c tweet)
     "Store tweets to REDIS. The tweets expire after 1 month"
     (let* [(serialized-tweet (serialize-tweet tweet))
            (vote-score 0)
@@ -114,24 +114,24 @@
            (redis-tweet-key (feed-tweet-hash serialized-tweet))
            (timeposted (feed-tweet-timeposted serialized-tweet))]
       (cond
-       [(not (redis-has-key? client (feed-tweet-hash serialized-tweet)))
-        (redis-hash-set! client redis-tweet-key "author" author)
-        (redis-hash-set! client redis-tweet-key "tweet" content)
-        (redis-hash-set! client redis-tweet-key "hash" redis-tweet-key)
-        (redis-hash-set! client redis-tweet-key "timeposted" timeposted)
-        (redis-hash-set! client redis-tweet-key "score" (number->string vote-score))
+       [(not (redis-has-key? c (feed-tweet-hash serialized-tweet)))
+        (redis-hash-set! c redis-tweet-key "author" author)
+        (redis-hash-set! c redis-tweet-key "tweet" content)
+        (redis-hash-set! c redis-tweet-key "hash" redis-tweet-key)
+        (redis-hash-set! c redis-tweet-key "timeposted" timeposted)
+        (redis-hash-set! c redis-tweet-key "score" (number->string vote-score))
         (redis-zset-add!
-         client
+         c
          "tweet-score:"
          redis-tweet-key
          vote-score)
         (redis-zset-add!
-         client
+         c
          "tweet-time:"
          redis-tweet-key
          timeposted-in-seconds)
         ;; Expire tweets after 1 week
-        (redis-expire-in! client redis-tweet-key (* 30 7 24 60 60 100))]
+        (redis-expire-in! c redis-tweet-key (* 30 7 24 60 60 100))]
        [else #f])))
   (cond
    [(and (list? tweets) (not (null? tweets)))
