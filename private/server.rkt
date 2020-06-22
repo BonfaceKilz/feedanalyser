915bf0d2d56bf634e2040c64598c67e72376abf7
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-06-23 02:57:36 +0300

    Update server code to use the updated methods

--- private/server.rkt
+++ private/server.rkt
@@ -16,12 +16,15 @@
          (let ([google-font-link "https://fonts.googleapis.com/css2?family=Amatic+SC&family=Josefin+Sans:ital,wght@1,300&display=swap"]
                [tweets (get-tweets/redis client)])
            (include-template "templates/polling.html"))))
+
   (post "/vote"
         (lambda (req)
           (let* ([json/vals (bytes->jsexpr (request-post-data/raw req))]
                  [tweet-hash (hash-ref json/vals 'tweet-hash)]
                  [upvote (hash-ref json/vals 'upvote)])
-            (vote-tweet client tweet-hash #:upvote? (string=? upvote "upvote"))
+            (vote-tweet! client tweet-hash #:upvote? (string=? upvote "upvote"))
             "OK")))
+
   (displayln (string-append "Running the server on port " (number->string port)))
+
   (run #:port port #:log-file log-file))
