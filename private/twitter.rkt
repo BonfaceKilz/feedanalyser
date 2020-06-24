c54b6560aa2411c601a44d40c731b0c091d127b3
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-06-24 18:52:57 +0300

    Revert "Feature/use twitter advanced search"

--- private/twitter.rkt
+++ private/twitter.rkt
@@ -1,41 +1,67 @@
 #lang racket
 
-(require gregor
-         racket/date
+(require racket/date
+         gregor
          redis)
-
 
 (provide get-tweets/redis
          get-tweets/twitter
-         store-tweets!
-         vote-tweet!
-         remove-expired-tweets!
-         remove-all-tweets!
-         (struct-out feed-tweet))
+         store-tweet
+         store-multiple-tweets
+         vote-tweet)
 
-
-; A simple (placeholder) tweet type, that contains metadata about a tweet
-(struct feed-tweet (author content timeposted hash) #:transparent)
-
-
-;; Check for tweets that have expired and remove them
-(define (remove-expired-tweets! client)
-  (let ([keys (redis-subzset
-               client
-               "tweet-score:"
-               #:start 0
-               #:stop -1)])
-    (map (lambda (key)
-           (unless (redis-has-key? client key)
+(define (remove-expired-tweets-from-zsets client)
+  (let [(tweet-scores (redis-subzset
+                       client
+                       "tweet-score:"
+                       #:start 0
+                       #:stop -1))]
+    (map (lambda (tweet-hash)
+           (unless (redis-has-key? client tweet-hash)
                ;;; Remove expired tweets from the relevant zsets
              (begin
-               (redis-zset-remove! client "tweet-score:" key)
-               (redis-zset-remove! client "tweet-time:" key))
-             (redis-hash-get client key)))
-         keys)))
+               (redis-zset-remove! client "tweet-score:" tweet-hash)
+               (redis-zset-remove! client "tweet-time:" tweet-hash))
+             (redis-hash-get client tweet-hash)))
+         tweet-scores)))
 
+(define (store-tweet client tweet-hash)
+  "Store tweets to REDIS. The tweets expire after 1 month"
+  (let* [(vote-score 0)
+         (tweet (car (hash-ref tweet-hash 'tweet)))
+         (author (car (hash-ref tweet-hash 'author)))
+         (timeposted (car (hash-ref tweet-hash 'timeposted)))
+         (timeposted-in-seconds (->posix (parse-datetime timeposted  "yyyy-MM-dd HH:mm:ss")))
+         (redis-tweet-key (string-append
+                "tweet:"
+                (number->string
+                 (equal-hash-code tweet))))]
 
-(define (get-raw-tweets userlist #:search-terms [search-terms #f] #:number [number 10])
+    ;; Remove any tweets that had expired from the zsets
+    (remove-expired-tweets-from-zsets client)
+
+    (if (not (redis-has-key? client redis-tweet-key))
+        (begin
+          (redis-hash-set! client redis-tweet-key "author" (string->bytes/utf-8 author))
+          (redis-hash-set! client redis-tweet-key "tweet" (string->bytes/utf-8 tweet))
+          (redis-hash-set! client redis-tweet-key "hash" redis-tweet-key)
+          (redis-hash-set! client redis-tweet-key "timeposted" (string->bytes/utf-8 timeposted))
+          (redis-hash-set! client redis-tweet-key "score" (number->string vote-score))
+          (redis-zset-add!
+           client
+           "tweet-score:"
+           redis-tweet-key
+           vote-score)
+          (redis-zset-add!
+           client
+           "tweet-time:"
+           redis-tweet-key
+           timeposted-in-seconds)
+          ;; Expire tweets after 1 week
+          (redis-expire-in! client redis-tweet-key (* 30 7 24 60 60 100)))
+        #f)))
+
+(define (get-raw-tweets name #:number [number 10])
   (let* [(n (if (string? number)
                number
                (number->string number)))
@@ -44,44 +70,26 @@
                    (lambda ()
                      (system
                       (string-append
-                       "twint --userlist "
-                       userlist
-                       (cond
-                        [search-terms
-                         (string-append " -s"
-                                        search-terms)]
-                        [else ""])
+                       "twint -s "
+                       name
                        " --limit "
                        n
                        " --link include"
                        " --format '{username} $@@$ {tweet} $@@$ {date} {time}||||||'"))))
                  "||||||"))]
-    (remove* (list "\n" '()) tweets)))
+    (remove* (list "\n" '()) tweets)
+    ))
 
-
-;; Get tweets from twitter and return them as strings. Search terms are search
-;; words that can used within twitter's own advanced search. `userlist' is a
-;; comma-separated list of users. It can be a single user though. Valid
-;; examples: "bonfacekilz"  "bonfacekilz,genenetwork2"
-(define (get-tweets/twitter userlist #:search-terms [search-terms #f] #:number [number 10])
+(define (get-tweets/twitter name #:number [number 10])
   "Get tweets from Twitter"
   (map
    (lambda (tweet)
      (let* [(ts (remove* (list "\n" '()) (string-split tweet "$@@$")))]
        (when (not (null? ts))
-         (define author (string-normalize-spaces (first ts)))
-         (define content (string-normalize-spaces (second ts)))
-         (define timeposted (->posix
-                             (parse-datetime
-                              (string-normalize-spaces (third ts))
-                              "yyyy-MM-dd HH:mm:ss")))
-         (define hash (string-append
-                       "tweet:"
-                       (number->string
-                        (equal-hash-code content))))
-         (feed-tweet author content timeposted hash))))
-   (get-raw-tweets userlist search-terms #:number number)))
-
+         (make-hash `((author ,(string-normalize-spaces (first ts)))
+                      (tweet ,(string-normalize-spaces (second ts)))
+                      (timeposted ,(string-normalize-spaces (third ts))))))))
+   (get-raw-tweets name #:number number)))
 
 (define (get-tweets/redis
          client
@@ -90,18 +98,17 @@
          #:stop [stop -1]
          #:reverse? [reverse? #t])
   "Get tweets from REDIS. Also removes expired tweets from the zsets"
-  (let ([tweet-scores (redis-subzset
+  (let [(tweet-scores (redis-subzset
                        client
                        key
                        #:start start
                        #:stop stop
-                       #:reverse? reverse?)])
+                       #:reverse? reverse?))]
     (map (lambda (tweet-hash)
            (redis-hash-get client tweet-hash))
          tweet-scores)))
 
-
-(define (vote-tweet! client tweet-hash #:upvote? [upvote #t])
+(define (vote-tweet client tweet-hash #:upvote? [upvote #t])
   (let* [(n (if upvote 1 -1))
          (key "tweet-score:")
          (score (string->number (bytes->string/utf-8
@@ -112,64 +119,7 @@
       (redis-zset-incr! client key tweet-hash (* n 1000)))))
 
 
-;; Given a list of feed-tweets, store them in REDIS
-(define (store-tweets! client tweets)
-  (define (store-tweet! c tweet)
-    "Store tweets to REDIS. The tweets expire after 1 month"
-    (let* [(serialized-tweet (serialize-tweet tweet))
-           (vote-score 0)
-           (author (feed-tweet-author serialized-tweet))
-           (content (feed-tweet-content serialized-tweet))
-           (redis-tweet-key (feed-tweet-hash serialized-tweet))
-           (timeposted (feed-tweet-timeposted serialized-tweet))]
-      (cond
-       [(not (redis-has-key? c (feed-tweet-hash serialized-tweet)))
-        (redis-hash-set! c redis-tweet-key "author" author)
-        (redis-hash-set! c redis-tweet-key "tweet" content)
-        (redis-hash-set! c redis-tweet-key "hash" redis-tweet-key)
-        (redis-hash-set! c redis-tweet-key "timeposted" timeposted)
-        (redis-hash-set! c redis-tweet-key "score" (number->string vote-score))
-        (redis-zset-add!
-         c
-         "tweet-score:"
-         redis-tweet-key
-         vote-score)
-        (redis-zset-add!
-         c
-         "tweet-time:"
-         redis-tweet-key
-         timeposted)
-        ;; Expire tweets after 1 week
-        (redis-expire-in! c redis-tweet-key (* 30 7 24 60 60 100))]
-       [else #f])))
-  (cond
-   [(not (null? tweets))
-    (for-each (lambda (tweet)
-                (store-tweet! client tweet))
-              (if (list? tweets)
-                  tweets
-                  `(,tweets)))
-    #t]
-   [else #f]))
-
-
-;; Serialize a tweet into a form that can be stored in REDIS
-(define (serialize-tweet tweet)
-  (feed-tweet (string->bytes/utf-8 (feed-tweet-author tweet))
-              (string->bytes/utf-8 (feed-tweet-content tweet))
-              (string->bytes/utf-8 (feed-tweet-timeposted tweet))
-              (string->bytes/utf-8 (feed-tweet-hash tweet))))
-
-;; Remove all tweets
-(define (remove-all-tweets! client)
-  (let ([keys (redis-subzset
-               client
-               "tweet-score:"
-               #:start 0
-               #:stop -1)])
-    (map (lambda (key)
-           (redis-zset-remove! client "tweet-score:" key)
-           (redis-zset-remove! client "tweet-time:" key)
-           (redis-remove! client key)
-           #t)
-         keys)))
+(define (store-multiple-tweets client name)
+  (for-each (lambda (tweet)
+              (store-tweet client tweet))
+            (get-tweets/twitter name)))
