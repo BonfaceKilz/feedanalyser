c21e1e4f3b87ddab64225c0417b5e0915f4c4a00
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-06-29 17:36:23 +0300

    Move conf varibles to conf file, but have defaults set

--- examples/update-feed.rkt
+++ examples/update-feed.rkt
@@ -9,23 +9,57 @@ This is a demo. Update as required!
 
 |#
 
-(require feedanalyser)
+(require racket/cmdline
+         feedanalyser)
 
-(define client (make-redis))
+
+;; Conf parameters
+(define conf-filepath (make-parameter #f))
+(define redis-conf (make-parameter '((host . "127.0.0.1")
+                                     (port . 6379)
+                                     (username . #f)
+                                     (password . #f))))
+(define refresh-time/hrs (make-parameter 24))
+
+;;; Default repos to fetch data from
+(define repos (make-parameter '(("genenetwork" . "genenetwork2")
+                                ("arvados" . "bh20-seq-resource"))))
+
+;;; Default params for twitter
+(define search-terms
+  (make-parameter "(genenetwork OR genenetwork2 OR rat OR mouse OR biology OR statistics) -Trump -trump"))
+(define twitter-users
+  (make-parameter "wolfgangkhuber,Y_Gliad,MarkGerstein,mstephens999,PaulFlicek,SagivShifman,Jericho,danjgaffney,bartdeplancke,robbie_stats,ClarissaCParker,DavidAshbrook,StatGenDan,GSCollins,MikeBradburn2,tobiaskurth,yudapearl,phuenermund"))
 
 
-;;; Config params for the tweets
-(define users "wolfgangkhuber,Y_Gliad,MarkGerstein,mstephens999,PaulFlicek,SagivShifman,Jericho,danjgaffney,bartdeplancke,robbie_stats,ClarissaCParker,DavidAshbrook,StatGenDan,GSCollins,MikeBradburn2,tobiaskurth,yudapearl,phuenermund")
+(define parser
+  (command-line
+   #:usage-help
+   "Start the update script"
+   #:once-each
+   [("-c" "--conf") filepath "Configuration file path"
+    (conf-filepath filepath)]
+   #:args () (void)))
 
-(define search-terms "(genenetwork OR genenetwork2 OR rat OR mouse OR biology OR statistics) -Trump -trump")
+(cond
+ [(conf-filepath)
+  (let [(server/settings
+         (load-config (conf-filepath)))]
+    (redis-conf (hash-ref server/settings 'redis-conf))
+    (refresh-time/hrs  (hash-ref server/settings 'refresh-time/hrs))
+    (repos (hash-ref server/settings 'repos))
+    (search-terms (hash-ref server/settings 'search-terms))
+    (twitter-users (hash-ref server/settings 'twitter-users)))])
+
+
+(define client (make-redis #:host (assoc-val 'host (redis-conf))
+                           #:port (assoc-val 'port (redis-conf))
+                           #:username (assoc-val 'username (redis-conf))
+                           #:password (assoc-val 'password (redis-conf))))
+
 
 (define (hours->seconds hours) (* hours 60 60))
 
-
-;;; Config params for the repos to fetch from
-(define repos '(("BonfaceKilz" . "feedanalyser")
-                ("genenetwork" . "genenetwork2")
-                ("arvados" . "bh20-seq-resource")))
 
 (void
  (thread
@@ -37,28 +71,26 @@ This is a demo. Update as required!
 (let loop ()
   ;; Adding tweets
   (displayln "Adding tweets:")
-
   (remove-expired-tweets! client)
-
   (store-tweets!
    client
-   (get-tweets/twitter users
-                       #:search-terms search-terms))
+   (get-tweets/twitter (twitter-users)
+                       #:search-terms (search-terms)))
+  (displayln "Done Adding tweets")
 
 
   ;; Adding commits
   (displayln "Adding commits:")
-
   (remove-expired-commits! client)
-
   (for-each
    (lambda (repo)
      (store-commits!
       client
       (get-commits/github (car repo)
                           (cdr repo))))
-   repos)
+   (repos))
+  (displayln "Done Adding commits")
 
-
-  (sleep (hours->seconds 12))
+  ;; Refresh after x hours
+  (sleep (hours->seconds (refresh-time/hrs)))
   (loop))
