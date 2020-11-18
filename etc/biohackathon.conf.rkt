3b99c8828dccb356d16bfcc4bba1956b6ef74bf5
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-11-18 20:13:01 +0300

    Add conf file for biohackathon website

---
+++ etc/biohackathon.conf.rkt
@@ -0,0 +1,23 @@
+;; PubSeq website conf file
+(redis-conf . '((host . "127.0.0.1")
+                (port . 6379)
+                (username . #f)
+                (password . #f)))
+
+;; Prefix to the redis columns
+(feed-prefix . "bh20-")
+
+;; The log file
+(log-file . "bh20-voting-server.log")
+
+;; The port to run the polling html page from
+(server-port . 8022)
+
+;; Use any twitter's advanced search options here
+(search-terms . "(covid OR covid19 OR corona) -Trump -trump")
+
+(twitter-users . "BonfaceKilz,wolfgangkhuber,Y_Gliad,MarkGerstein,mstephens999,PaulFlicek,SagivShifman,Jericho,danjgaffney,bartdeplancke,robbie_stats,ClarissaCParker,DavidAshbrook,StatGenDan,GSCollins,MikeBradburn2,tobiaskurth,yudapearl,phuenermund,pjotrprins,GeneNetwork2")
+
+
+;; All the repos you want to fetch commits from
+(repos . '(("arvados" . "bh20-seq-resource")))
