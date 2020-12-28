a7192c5cb707fbe2522471d225f4c9101b9d39a5
BonfaceKilz <me@bonfacemunyoki.com>
2020-12-28 21:19:16 +0300

    tests: pubmed-test: Remove test for "serialize-pubmed-feed"

--- tests/pubmed-test.rkt
+++ tests/pubmed-test.rkt
@@ -41,9 +41,3 @@
                 "Response to COVID-19 in Cyprus: policy changes and epidemic trends."
                 "33338320"))
               "Check correct struct")
-
-
-(let ([test-feed-struct (feed-pubmed "a" "b" "c" "d" "e" "f")])
-  (check-equal?
-   (serialize-pubmed-feed test-feed-struct)
-   (feed-pubmed #"a" #"b" #"c" #"d" #"e" #"f")))
