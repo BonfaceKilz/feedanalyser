db589b0bc44bd2ef8e693627867128f40f979a5a
BonfaceKilz <me@bonfacemunyoki.com>
2021-01-04 16:15:17 +0300

    etc: genenetwork.conf: Update some params for gn2 server

--- etc/genenetwork.conf.rkt
+++ etc/genenetwork.conf.rkt
@@ -11,18 +11,27 @@
 (tweets-per-user . 2)
 
 ;; The log file
-(log-file . "voting-server.log")
+(log-file . "gn2-voting-server.log")
 
 ;; The port to run the voting.html page from
-(server-port . 6011)
+(server-port . 8023)
 
 ;; Use any twitter's advanced search options here
-(twitter-search-terms . "(genenetwork OR genenetwork2 OR rat OR mouse OR biology OR statistics) -Trump -trump")
+(twitter-search-terms . "(genenetwork OR genenetwork2 OR rat OR mouse OR pangenome OR browser OR workflows OR bioinformatic OR GeneNetwork OR conference OR sequences OR genotype OR phenotype OR guix OR RDF OR SPARQL OR genome OR biology OR Genomics OR OPAR OR genetics OR research OR ontologies OR variation)")
 
-(pubmed-search-terms . "(genenetwork OR genenetwork2 OR rat OR mouse OR biology OR statistics)")
+(pubmed-search-terms . "(genenetwork OR genenetwork2 OR rat OR mouse OR pangenome OR genome browser)")
+
+(arxiv-search-terms . '((AND genenetwork title)
+                        (OR genenetwork2 abstract)
+                        (OR genome-browser abstract)
+                        (OR pangenome abstract)
+                        (OR mouse title)))
+
+(min-retweets . 4)
 
 ;; Have a list of users from which you can fetch tweets from
-(twitter-users . "wolfgangkhuber,Y_Gliad,MarkGerstein,mstephens999,PaulFlicek,SagivShifman,Jericho,danjgaffney,bartdeplancke,robbie_stats,ClarissaCParker,DavidAshbrook,StatGenDan,GSCollins,MikeBradburn2,tobiaskurth,yudapearl,phuenermund,pjotrprins,GeneNetwork2")
+(twitter-users . "wolfgangkhuber,Y_Gliad,MarkGerstein,mstephens999,PaulFlicek,SagivShifman,Jericho,danjgaffney,bartdeplancke,robbie_stats,ClarissaCParker,DavidAshbrook,StatGenDan,GSCollins,MikeBradburn2,tobiaskurth,yudapearl,phuenermund,pjotrprins,GeneNetwork2,commonwl,simonheumos,gedankenstuecke,DCGenomics,pvanheus,miles_benton,wait_sasha,ewanbirney,BenedictPaten,sjackman,webyrd,BioMickWatson,drtkeane,GA4GH,KaczorowskiLab,AMelquiond,justsaysinmice,OPARProject,erikgarrisonDannyArends,JamesEKrause,Eric_Fauman,jlane_boston,arcova,FrontGenetics,AngelicaLeeOli1,bonfacekilz,fermatslibrary,arxiv,francois_sabot,metegenomez,MColebrook,GenoLabgem,pangenomepapers")
+
 
 ;; All the repos you want to fetch commits from
 (repos . '(("genenetwork" . "genenetwork2")))
